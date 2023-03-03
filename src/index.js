ec10d6651859c95e463d91bb1795c7e1a43c736e
sarah <skg@neofilm.us>
2023-03-03 17:33:19 -0500

    Render nametag in App component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import NameTag from "./NameTag.js";
-import "./styles.css";
 import { names } from "./data.js";
+import "./styles.css";
 
 const renderNameTag = (name) => <NameTag name={name.person} key={name.id} />;
 
-const App = () => (
+const App = () => {
   const NameTagElements = names.map(renderNameTag);
-  <div className="App">
-    <h1>Name Tag Generator</h1>
-    <NameTag name="Sarah" />
-    <NameTag name="Jeff" />
-    <NameTag name="William" />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Name Tag Generator</h1>
+      {NameTagElements}
+    </div>
+  );
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
