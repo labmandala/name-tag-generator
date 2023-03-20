abca5c811f4b3abcc3e11db49040a9cbf07aabca
sarah <skg@neofilm.us>
2023-03-20 13:44:00 -0400

    Replace data.js w/ state property in component

--- src/data.js
+++
@@ -1,5 +0,0 @@
-export const names = [
-    { person: "Sarah", id: "001"},
-    { person: "William", id: "002"},
-    { person: "Jeff", id: "003"}
-];
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import NameTag from "./NameTag.js";
-import { names } from "./data.js";
+
 import "./styles.css";
 
-const renderNameTag = (name) => <NameTag name={name.person} key={name.id} />;
+const renderNameTag = (name) => <NameTag name={name} key={name} />;
 
 class App extends Component {
+
+  state = { names: ["Sarah", "William", "Jeff"]}
+
   render() {
-    const NameTagElements = names.map(renderNameTag);
+    const NameTagElements = this.state.names.map(renderNameTag);
     return (
       <div className="App">
         <h1>Name Tag Generator</h1>
