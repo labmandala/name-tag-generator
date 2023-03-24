19301f7f2295dffcd145c39c6188ff6c4d9ad623
sarah <skg@neofilm.us>
2023-03-24 13:36:47 -0400

    Initialize state w/ name, ensure input displays it

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 
 class UserInput extends Component {
+
+  state = {
+    name: ""
+  };
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Add a new name here..." />
+        <input 
+          type="text" 
+          placeholder="Add a new name here..." 
+          value={this.state.name}
+          />
         <input type="submit" value="Create Name Tag" />
       </form>
     );
