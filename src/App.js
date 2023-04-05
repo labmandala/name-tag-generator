da39cecde974236c20b8baef3decc85886a916d1
sarah <skg@neofilm.us>
2023-04-05 16:11:30 -0400

    Update main app code & styles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ class App extends Component {
     names: ["Erin", "Ann", "Nichole", "Sharon", "Maryn"]
   };
   removeName = (clickedIndex) => {
-    // to learn how the .filter method works, check out https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
     const filterCallback = (_, index) => index !== clickedIndex;
     const newNames = this.state.names.filter(filterCallback);
     this.setState({ names: newNames });
